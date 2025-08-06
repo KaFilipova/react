fab860ab96ccd14df6e347e7eb3b8fac36c352ea
Karinushka <4kafilipova@gmail.com>
2025-08-07 01:00:19 +0400

    how to write React Fragment

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,42 @@
 import { createElement, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-// jsx
+// import App from './App.tsx'
 
-const h1 = <h1 id="title">Hello React.js</h1>
-let a = 10
-let b = 'hello test'
-const list = (
-    <ul>
-        <li>listitem1 {a + 10 + 10}</li>
-        <li>listitem2 {b}</li>
-    </ul>
-)
+// компоненты пишутся с большой буквы. компоненты вызываются как теги </>
 
-const content = (
-    <div>
-        {h1}
-        {list}
-    </div>
-)
-
+const Title = () => {
+    return <h1>Hello App.js</h1>
+}
+const Content = () => {
+    return (
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                vel distinctio veniam odit quas suscipit veritatis
+                necessitatibus, impedit at, praesentium sapiente error dicta
+                blanditiis earum reprehenderit mollitia recusandae maxime
+                aliquid.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Blanditiis tempora fuga, quas laboriosam dolore cumque, sed
+                optio aspernatur impedit expedita officiis animi odio id maiores
+                ipsa, accusamus nesciunt quo! Quidem?
+            </p>
+        </>
+    )
+}
+function App() {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
+}
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>{list}</StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
