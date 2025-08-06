d2c1af3ac60b69521466fd9b7ce9add0c8a9caf9
Karinushka <4kafilipova@gmail.com>
2025-08-06 14:57:59 +0400

    old type of creating react elements

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+import { createElement, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const h1 = createElement('h1', { id: 'title' }, 'Hello React')
+const list = createElement('ul', null, createElement('li', null, 'listitem'))
+const content = (
+    <div>
+        {h1}
+        {list}
+    </div>
+)
+
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+    <StrictMode>{content}</StrictMode>
 )
