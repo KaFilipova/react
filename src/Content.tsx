fb299b178b9397f7612bd3c888526b8d3f9d875e
Karinushka <4kafilipova@gmail.com>
2025-09-02 00:21:48 +0400

    способ использовать класс с одным и тем же названием но с индивидуальным контентом с помощью хеш-функции

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import './Content.css'
+import classes from './Content.module.css'
 
 type Props = { text1: string; text2: string; year: number }
-
+console.log(classes)
 export const Content = (props: Props) => {
     let data = true
     return (
         <>
-            <p
-                style={{
-                    color: data ? 'yellow' : 'white',
-                    backgroundColor: 'green',
-                }}
-            >
-                {props.text1}
-            </p>
-            <p>{props.text2}</p>
+            <p>{props.text1}</p>
+            <p className={`${classes.content}`}> {props.text2}</p>
             <div> year first = {props.year}</div>
         </>
     )
